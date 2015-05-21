d632853400a312814dd1335d02a8788c71aa1e93
totaldis <totaldis@gmail.com>
2015-05-20 23:16:06 -0400

    new appjs for api

--- api/app.js
+++ api/app.js
@@ -1,1 +1,16 @@
 // api app
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3030;
+
+var router = express.Router();
+
+
+app.get('/', function(req, res) {
+	res.send('API test');
+});
+
+app.listen(port, function() {
+	console.log("API Server started on port " + port + "...");
+});
