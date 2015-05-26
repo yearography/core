cc5837aca80bcbfbea6431c733a2ec3619a5ea88
Aaron VanEtveldt <totaldis@gmail.com>
2015-05-25 22:38:30 -0400

    Yeoman angular template with modifications using ui-route.

--- client/app/scripts/app.config.js
+++ client/app/scripts/app.config.js
@@ -1,4 +1,5 @@
 'use strict';
+// see http://www.funnyant.com/angularjs-ui-router/
 
 angular.module('clientApp').config(function($urlRouterProvider, $stateProvider) {
 	// default missing route
@@ -6,9 +7,39 @@ angular.module('clientApp').config(function($urlRouterProvider, $stateProvider) 
 		.otherwise('/');
 
 	$stateProvider
+		// all states that compose the entire application will inherit from the app state
+		// which includes a common header and common footer
+		.state('app', {
+			url: '/app',
+			views: {
+				'header': {
+					templateUrl: '/views/partials/app_header.html'
+				},
+				'content': {
+					templateUrl: '/views/main.html'
+				},
+				'footer': {
+					templateUrl: '/views/partials/app_footer.html'
+				}
+			}
+		})
+		// this state is nested within app and inherits it's views unless explicitly overriden
+		.state('app.photos', {
+			url: '/photos',
+			views: {
+				'content@': {
+					templateUrl: '/views/photos.html'
+				}
+			}
+		})
+		// the landing page is not attached to the app state and therefore lives on the same level
+		// if a view is omitted it will not inherit, it simply won't appear
 		.state('landing', {
 			url: '/',
 			views: {
+				'header': {
+					templateUrl: '/views/partials/landing_header.html'
+				},
 				'content': {
 					templateUrl: '/views/landing.html'
 				}
